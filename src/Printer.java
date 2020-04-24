d858005cdcf9d32ae2179b02c49ca7d12563abfc
Vladimir Baykalov <self.awarenesse@gmail.com>
2020-04-24 16:12:51 +0300

    Abstarct Printer and CanonPrinter

--- src/Printer.java
+++ src/Printer.java
@@ -1,18 +1,5 @@
-public class Printer implements DocsPrint {
-    public static void main(String[] args) {
-        Printer canonPrinter = new Printer();
-        canonPrinter.printPdfFile();
-        canonPrinter.printWordFile();
-
-    }
-    @Override
-    public void printPdfFile() {
-        System.out.println("PDF файл печатается");
-    }
-
-    @Override
-    public void printWordFile() {
-        System.out.println("Word файл печатается");
-    }
+public abstract class Printer {
+    public abstract void printPdfFile();
+    public abstract void printWordFile();
 
 }
